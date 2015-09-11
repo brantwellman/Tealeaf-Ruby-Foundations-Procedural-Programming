7f8f42fcf78f52734568f73a63b5df36fb0901e9
Brant Wellman <BrantWellman@gmail.com>
2015-09-11 12:53:22 -0700

    Complete Pseudo Code lesson

---
+++ lesson_2/pseudo.rb
@@ -0,0 +1,41 @@
+# method that returns the sum of two integers
+# Given two integers
+#   - Add the two integers
+#   - Return the sum
+
+# START
+# Given two integers
+# Sum the integers
+# END
+#####
+
+# method that takes an array of strings, and returns a string that is all those strings concatenated together
+# Given an array of strings
+# Iterate over through that array
+#   - Remove the string from the array
+#   - Save the removed string to a new string
+
+# START
+# Given an array of strings
+# SET empty string variable
+# Iterate through each of the strings
+#   - Remove the string from the array
+#   - Push it on to the string variable
+# Return string variable
+# END
+#####
+
+# method that takes an array of integers and returns a new array with every other element
+# Given an array of integers
+# Iterate through the array
+#   - Check the index of the item
+#   - If the index is even add the integer to a new array
+
+# START
+# Given an array of integers
+# SET an empty array variable
+# Iterate through eatch of the integers
+#   - IF the index == even
+#     - Add integer to new array
+# Return array variable
+# END
