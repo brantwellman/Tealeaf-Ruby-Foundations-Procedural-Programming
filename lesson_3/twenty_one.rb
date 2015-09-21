983d001fdb3ba273e43c01b028e6f7798e352c0c
Brant Wellman <BrantWellman@gmail.com>
2015-09-21 15:57:42 -0700

    Refactored 21 after Lesson 8

--- lesson_3/twenty_one.rb
+++ lesson_3/twenty_one.rb
@@ -7,20 +7,11 @@ end
 def initialize_deck
   suits = ['C', 'D', 'H', 'S']
   values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-  values.product(suits)
-end
-
-def deal_initial_cards(deck, dealer_hand, player_hand)
-  player_hand << deck.shuffle!.pop
-  dealer_hand << deck.shuffle!.pop
-  player_hand << deck.shuffle!.pop
-  dealer_hand << deck.shuffle!.pop
-  return deck
+  values.product(suits).shuffle
 end
 
 def deal_card(deck, hand)
-  hand << deck.shuffle!.pop
-  return deck
+  hand << deck.pop
 end
 
 def display_cards(dealer_hand, player_hand)
@@ -94,75 +85,107 @@ end
   # end
 # end
 
-def player_win?(dealer_hand, player_hand)
-  total(player_hand) > total(dealer_hand)
+def result?(dealer_hand, player_hand)
+  player_total = total(player_hand)
+  dealer_total = total(dealer_hand)
+
+  if player_total > 21
+    :player_busted
+  elsif dealer_total > 21
+    :dealer_busted
+  elsif dealer_total >= player_total
+    :dealer
+  elsif player_total > dealer_total
+    :player
+  end
 end
 
 def display_winner(dealer_hand, player_hand)
-  if player_win?(dealer_hand, player_hand)
-    prompt "Your total is #{total(player_hand)} and the Dealer's total is #{total(dealer_hand)}. You win!"
-  else
+  result = result?(dealer_hand, player_hand)
+
+  case result
+  when :player_busted
+    prompt "You busted. Dealer wins."
+  when :dealer_busted
+    prompt "The Dealer busted. You win!"
+  when :dealer
     prompt "Your total is #{total(player_hand)} and the Dealer's total is #{total(dealer_hand)}.  Sorry, the dealer won."
+  when :player
+    "Your total is #{total(player_hand)} and the Dealer's total is #{total(dealer_hand)}. You win!"
   end
 end
 
+def play_again?
+  prompt "Do you want to play again? (y or n)"
+  gets.chomp.downcase.start_with?('y')
+end
 
 prompt "Welcome to my fancy dancy game of Twenty-one!"
 
-player_cards = []
-dealer_cards = []
-
-
-new_deck = initialize_deck
-
-deal_initial_cards(new_deck, player_cards, dealer_cards)
-
-display_cards(dealer_cards, player_cards)
-
-# player_turn(new_deck, player_cards)
-
-answer = ''
 loop do
-  prompt "Would you like to Hit (H) or Stay (S)? Please enter one."
-  answer = gets.chomp.downcase
-  if !answer.start_with?('s', 'h')
-    prompt "That was an improper response."
-  elsif answer.start_with?('h')
+
+  player_cards = []
+  dealer_cards = []
+  new_deck = initialize_deck
+
+  2.times do
     deal_card(new_deck, player_cards)
-    prompt "You were dealt a #{player_cards.last[0]}."
-    prompt "Your total is #{total(player_cards)}"
-      if busted?(player_cards)
-       prompt "You busted. The Dealer wins. :("
-       break
-       return
-     end
-  else
-    break
+    deal_card(new_deck, dealer_cards)
   end
-end
 
-return if busted?(player_cards)
+  display_cards(dealer_cards, player_cards)
 
+  # player_turn(new_deck, player_cards)
+  loop do
+    answer = ''
+    loop do
+      prompt "Would you like to Hit (H) or Stay (S)? Please enter one."
+      answer = gets.chomp.downcase
+      break if ['h', 's'].include?(answer)
+      prompt "That was an improper response."
+    end
 
-# dealer_turn(new_deck, dealer_cards)
+    if answer == 'h'
+      deal_card(new_deck, player_cards)
+      prompt "You chose to hit."
+      prompt "You were dealt a #{player_cards.last[0]}."
+      prompt "Your total is #{total(player_cards)}"
+    end
 
-prompt "The Dealer has a #{dealer_cards[0][0]} and the hole card is a #{dealer_cards[1][0]}."
-loop do
-  if total(dealer_cards) >= 17
-    prompt "The dealer stays."
-    break
+    break if answer == 's' || busted?(player_cards)
+  end
+
+  if busted?(player_cards)
+    display_winner(dealer_cards, player_cards)
+    play_again? ? next : break
   else
+    prompt "You stayed and your total is #{total(player_cards)}"
+  end
+
+  # dealer_turn(new_deck, dealer_cards)
+  prompt "Now its the Dealer's turn..."
+  prompt "The Dealer has a #{dealer_cards[0][0]} and the hole card is a #{dealer_cards[1][0]}."
+  loop do
+    break if busted?(dealer_cards) || total(dealer_cards) >= 17
+    prompt "The Dealer hits"
     deal_card(new_deck, dealer_cards)
     prompt "The Dealer dealt himself a #{dealer_cards.last[0]}."
     prompt "The Dealer's total is #{total(dealer_cards)}."
-    if busted?(dealer_cards)
-      prompt "The Dealer busted. You win!"
-      break
-      return
-    end
   end
+
+  if busted?(dealer_cards)
+    display_winner(dealer_cards, player_cards)
+    play_again? ? next : break
+  else
+    prompt "Dealer stays and his total is #{total(dealer_cards)}"
+  end
+
+  prompt "Both have stayed."
+  prompt "The Player's total is #{total(player_cards)}."
+  prompt "The Dealer's total is #{total(dealer_cards)}."
+  display_winner(dealer_cards, player_cards)
+
+  break unless play_again?
 end
 
-player_win?(dealer_cards, player_cards)
-
-display_winner(dealer_cards, player_cards)
+prompt "Thanks for playing my fancy dancy game of Twenty-one!"
